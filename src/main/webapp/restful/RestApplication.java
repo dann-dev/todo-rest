22ef401cfd3ebc0a5319cebc4b2a2c36ae1b1af1
dann-dev <dann-dev@users.noreply.github.com>
2015-07-22 09:24:06 +1200

    Stuff

--- src/main/webapp/restful/RestApplication.java
+++ src/main/webapp/restful/RestApplication.java
@@ -3,7 +3,7 @@ package main.webapp.restful;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
-@ApplicationPath("/restApp")
+@ApplicationPath("/restapp")
 public class RestApplication extends Application{
 
 }
